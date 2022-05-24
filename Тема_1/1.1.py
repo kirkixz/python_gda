9b44f2fc8e0a10f1a73479829bcc6094265f2f49
Kirkixz <kirkixzz@gmail.com>
2022-05-24 23:33:57 +0300

    Complite 1.1

--- Тема_1/1.1.py
+++ Тема_1/1.1.py
@@ -1,1 +1,4 @@
-print("Hello world!")
+full_name = input("Введите ваше ФИО: ")
+address = input("Введите ваш адрес: ")
+
+print(f"Здравствуйте, {full_name.title()}. Ваш заказ отправлен по адресу {address.title()}")
