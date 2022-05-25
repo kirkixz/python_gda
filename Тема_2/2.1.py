e89511ea275affc11990df66c2c10c23c8f7ef5b
Kirkixz <kirkixzz@gmail.com>
2022-05-25 18:10:11 +0300

    Complite 2.1

---
+++ Тема_2/2.1.py
@@ -0,0 +1,9 @@
+temperature_room = float(input("Введите температуру в комнате "))
+desired_temperature = float(input("Введите желаемую температуру "))
+humidity_air = int(input("Введите влажность воздуха "))
+
+if temperature_room > desired_temperature and humidity_air < 80:
+    print("on")
+else:
+    print("off")
+
