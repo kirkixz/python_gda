cd545dbc5705ec0b7ea8ae2452d83c6e61bb3f6d
Kirkixz <kirkixzz@gmail.com>
2022-10-24 17:55:18 +0300

    Complite 6.2

--- Тема_6/6.2.py
+++ Тема_6/6.2.py
@@ -1,15 +1,3 @@
-# Напишите класс ListWorker:
-#
-# принимает в себя неограниченное количество неименованных аргументов и работает с ними как со списком
-#
-# реализуйте методы, которые возвращают
-#
-# только числа
-#
-# только строки
-#
-# все кроме чисел и строк
-
 class ListWorker:
     def __init__(self, *args):
         self.args = args
@@ -29,6 +17,7 @@ class ListWorker:
                 self.lst.append(value)
         return self.lst
 
+# Метод, который возвращвет все кроме чисел и строк
     def print_other(self):
         for value in self.args:
             if not isinstance(value, (int, float, str)):
@@ -36,14 +25,14 @@ class ListWorker:
         return self.lst
 
 
+print(ListWorker(1, 2, 3, 4, 5, 's', 'str', 'num', 'raw', [1, 2, 3, '&', 'f'], (2, 5, 'h'), {'wer': 54, 'raw': 65}).print_nums())
+print(ListWorker(1, 2, 3, 4, 5, 's', 'str', 'num', 'raw', [1, 2, 3, '&', 'f'], (2, 5, 'h'), {'wer': 54, 'raw': 65}).print_str())
+print(ListWorker(1, 2, 3, 4, 5, 's', 'str', 'num', 'raw', [1, 2, 3, '&', 'f'], (2, 5, 'h'), {'wer': 54, 'raw': 65}).print_other())
+
 # 1, 2, 3, 4, 5,                    #числа
 # 's', 'str', 'num', 'raw',         #строки
 # [1, 2, 3, '&', 'f'],              #список
 # (2, 5, 'h'),                      #кортеж
 # {'wer': 54, 'raw': 65}            #словарь
 
-example = 1, 2, 3, 4, 5, 's', 'str', 'num', 'raw', [1, 2, 3, '&', 'f'], (2, 5, 'h'), {'wer': 54, 'raw': 65}
-
-print(ListWorker(example).print_nums())
-# print(ListWorker(example).print_str())
-# print(ListWorker(example).print_other())
+# example = 1, 2, 3, 4, 5, 's', 'str', 'num', 'raw', [1, 2, 3, '&', 'f'], (2, 5, 'h'), {'wer': 54, 'raw': 65}
