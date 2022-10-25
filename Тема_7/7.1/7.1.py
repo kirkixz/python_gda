7d01a23da870123e0a9c7b28a65a6d5dcfd53ac2
Kirkixz <kirkixzz@gmail.com>
2022-10-25 22:34:23 +0300

    Complite 7.1

--- Тема_7/7.1/7.1.py
+++ Тема_7/7.1/7.1.py
@@ -1,7 +1,7 @@
-def get_six_ln():
+def print_six_ln():
     with open(r"C:\Users\user\Desktop\data.txt", mode='r', encoding='utf-8') as f:
         for i in range(6):
             print(f.readline().strip())
 
 
-get_six_ln()
+print_six_ln()
