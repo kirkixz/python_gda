39ded33011f76bb9ecfbc9a61d465f49492971b7
Kirkixz <kirkixzz@gmail.com>
2022-10-28 22:32:16 +0300

    Complite 7.2

--- Тема_7/7.2.py
+++ Тема_7/7.2.py
@@ -1,15 +1,10 @@
 def read_last(lines, file):
     with open(file=file, mode='r', encoding='utf-8') as f:
-        # for line in range(-1, -lines - 1, -1):
-        #
-        #     print(f.readlines()[line])
-        for line in range(lines):
-            counter = -1
-            while counter != -lines:
-                print(f.readline()[counter].strip())
-                counter = counter - 1
-                print(counter)
-                break
+        text = f.readlines()
+        text.reverse()
+
+        for i in range(lines):
+            print(text[i].strip())
 
 
-read_last(3, r"C:\Users\user\Desktop\data.txt")
+read_last(5, r"C:\Users\user\Desktop\data.txt")
