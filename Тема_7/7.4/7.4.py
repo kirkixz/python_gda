d235aa0250300c31b5513bd825c6cb9aaf5d28a3
Kirkixz <kirkixzz@gmail.com>
2022-10-29 01:13:33 +0300

    Complite 7.4

--- Тема_7/7.4/7.4.py
+++ Тема_7/7.4/7.4.py
@@ -1,13 +1,13 @@
 def print_word_count(file, search_word) -> int:
     marks = '!();?:,.'
     with open(file=file, mode='r', encoding='utf-8') as f:
-        temp = f.readlines()
+        text = f.readlines()
         counter = 0
 
-        for i in range(len(temp)):
-            temp[i] = temp[i].strip().lower().split(' ')
+        for i in range(len(text)):
+            text[i] = text[i].strip().lower().split(' ')
 
-            for j in temp[i]:
+            for j in text[i]:
                 for char in marks:
                     j = j.replace(char, '')
                 if j == search_word.lower().strip():
