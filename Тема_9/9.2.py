aab9d8036d656209c14a39e75b7e5a1fbe2bb347
Kirkixz <Kirkixzz@gmail.com>
2022-12-15 00:25:54 +0300

    Complite 9.2

--- Тема_9/9.2.py
+++ Тема_9/9.2.py
@@ -1,3 +1,6 @@
+import requests
+
+
 def url_address(url: str, array_parameters: list) -> str:
 
     # array_parameters = ["true", "new", "tshort"]
@@ -16,3 +19,22 @@ def url_address(url: str, array_parameters: list) -> str:
 
 
 print(url_address("https://www.ozon.ru/search/?from_global=@&sorting=@&text=@", ["true", "rating", "minecraft"]))
+
+
+def url_address(url: str, from_global: str, sorting: str, text: str) -> str:
+
+    array_parameters = {"from_global": from_global, "sorting": sorting, "text": text}
+
+    # Second parameter:
+    #     new
+    #     ozon_card_price
+    #     price_desc
+    #     discount
+    #     rating
+
+    url = requests.get(url, params=array_parameters)
+
+    return url.url
+
+
+print(url_address("https://www.ozon.ru/search/", from_global="true", sorting="rating", text="minecraft"))
